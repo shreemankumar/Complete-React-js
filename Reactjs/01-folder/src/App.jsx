9f2e931db576e18e9c4b5c2935593eb4a6358e9e
Shreeman Badatya <bshreemankumar@gmail.com>
2026-01-03 23:04:50 +0530

    Fragment : <></>, it is wrapper

--- Reactjs/01-folder/src/App.jsx
+++ Reactjs/01-folder/src/App.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      <h1>shreeman</h1>
-      <h1>badatya</h1>
+   <>
+    <div id='parent'>
+      <h1 id='child1'>shreeman</h1>
+      <h1 id='child2'>badatya</h1>
     </div>
+    <div><h1>learn react js</h1></div>
+   </>
   )
 }
 
