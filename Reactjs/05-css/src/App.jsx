a4e112c40455f3da8c8fc1a58ad20b7c4e6215be
Shreeman Badatya <bshreemankumar@gmail.com>
2026-01-04 23:58:18 +0530

    Another type of writing css

--- Reactjs/05-css/src/App.jsx
+++ Reactjs/05-css/src/App.jsx
@@ -5,8 +5,8 @@ import Header from './components/Header'
 const App = () => {
   return (
     <div>
-      <Button />
       <Header />
+      <Button />
     </div>
   )
 }
--- Reactjs/05-css/src/components/Button.jsx
+++ Reactjs/05-css/src/components/Button.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import '../styles/Button.css'
 const Button = () => {
   return (
     <div className='btn'>
