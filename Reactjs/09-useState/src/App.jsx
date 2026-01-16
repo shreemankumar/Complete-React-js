38b044819ac688228ed154e2e381d28d5d3dd68b
Shreeman Badatya <bshreemankumar@gmail.com>
2026-01-16 23:44:19 +0530

    learning about Hooks & useState

--- Reactjs/09-useState/src/App.jsx
+++ Reactjs/09-useState/src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import { useState } from 'react'
 const App = () => {
+  //useState having two elements, first one is the variable Read only and the second one is for write only for update.
+  const [Download, setDownload] = useState('download')
   return (
-    <div>App</div>
+    <div>
+      <h1>Click the button to get {Download}</h1>
+       <button onClick={() => {
+        setDownload("done !")
+       }}>Download</button>
+      
+    </div>
   )
 }
 
