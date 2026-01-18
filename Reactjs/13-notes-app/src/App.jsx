7e8e668358caa04d2aa66dba217b747106b52a95
Shreeman Badatya <bshreemankumar@gmail.com>
2026-01-18 17:06:42 +0530

    Delete button add & finished

--- Reactjs/13-notes-app/src/App.jsx
+++ Reactjs/13-notes-app/src/App.jsx
@@ -13,15 +13,19 @@ const App = () => {
     const copyTasks = [...Show]
     copyTasks.push({first, details})
     setShow(copyTasks);
-    console.log(copyTasks);
-    
-    
     setfirst('')
     setDetails('')
   }
 
+  const deleteNote = (idx) => {
+    const copyTask = [...Show];
+    copyTask.splice(idx, 1);
+    setShow(copyTask);
+    
+  }
+
   return (
-    <div className='overflow-y-auto h-screen bg-gray-900 text-white p-10 gap-8 lg:flex'>
+    <div className=' overflow-y-auto h-screen bg-gray-900 text-white p-10 gap-8 lg:flex'>
       <form 
         onSubmit={submitHandler} 
         className='flex flex-col gap-4 items-start lg:w-1/2 p-10' 
@@ -51,40 +55,45 @@ const App = () => {
       <div className=' p-10 lg:border-l-2 lg:w-1/2'>
       <h1 className='text-3xl font-bold'>Notes</h1>
       <div className=' flex gap-2 overflow-auto no-scrollbar flex-wrap mt-5 h-full p-4'>
-      {Show.map((elem) => (
+      {Show.map((elem,idx) => (
         <div
-  key={elem.id}
-  className="
-    px-4 py-4
-    h-56 w-52
-    rounded bg-cover bg-center
-    bg-[url(https://plus.unsplash.com/premium_photo-1751106852077-5b918fcb195d)]
-  "
->
-  <h3 className="leading-tight wrap-break-word font-bold text-black">
-    {elem.first}
-  </h3>
+          key={idx}
+          className="
+            flex flex-col 
+            justify-between
+            items-start relative
+            px-4 py-4
+            h-60 w-52
+            rounded bg-cover bg-center
+            bg-[url(https://plus.unsplash.com/premium_photo-1751106852077-5b918fcb195d)]
+          "
+        >
+          <h3 className="leading-tight wrap-break-word font-bold text-black">
+            {elem.first}
+          </h3>
 
-  <p
-    className="
-      mt-3
-      h-36
-      overflow-y-auto
-      overflow-x-hidden
-      wrap-break-word
-      leading-relaxed
-      font-medium
-      text-gray-700
-      no-scrollbar
-    
-    "
-  >
-    {elem.details}
-  </p>
-</div>
-))}
-
- 
+          <p
+            className="
+              mt-3
+              h-36
+              overflow-y-auto
+              overflow-x-hidden
+              wrap-break-word
+              leading-tight
+              
+              font-medium
+              text-gray-700
+              no-scrollbar
+              
+            "
+          >
+            {elem.details}
+          </p>
+          <button onClick={() => {
+            deleteNote(idx)
+          }} className='bg-red-500 w-full active:scale-95 rounded py-2 text-xs cursor-pointer'>Delete</button>
+        </div>
+        ))}
       </div>
       </div>
     </div>
