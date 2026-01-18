07f1987a491d2b3a16acf84b53a840ee58d609f2
Shreeman Badatya <bshreemankumar@gmail.com>
2026-01-18 22:59:08 +0530

    local storage, .setItem, .getItem, .clear, .removeItem, concept of JSON.stringify and JSON.parse (with array & object)

--- Reactjs/14-localstorage/src/App.jsx
+++ Reactjs/14-localstorage/src/App.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
 
 const App = () => {
+  const showData = () => {
+    // localStorage.setItem('name','shreeman')
+    // localStorage.setItem('age',21)
+    // const name = localStorage.getItem('name')
+    // const age = localStorage.getItem('age')
+    // console.log(name);
+    // console.log(age);
+    // localStorage.clear();  // for clear all the value form the local storage
+    //localStorage.removeItem('name');
+    //console.log(name);
+    
+    // from the array
+    const arrayShow = {
+      'address' : 'gajapati nagar',
+      'pincode' : 760010
+    }
+    localStorage.setItem('userAddress',JSON.stringify(arrayShow))
+   const userData = localStorage.getItem('userAddress')
+   const data = JSON.parse(userData);
+   console.log(data.address);
+   console.log(data.pincode);
+   
+    
+
+
+    
+  }
   return (
-    <div>App</div>
+    <div>
+      <button className='btn' onClick={showData}>Click</button>
+    </div>
   )
 }
 
